c264ca5ffa3d8b9aac298a8a25207a3eecc951af
925014380@qq.com <925014380@qq.com>
2017-08-19 10:29:20 +0800

    yyyy

---
+++ 移动端项目开发/index.js
@@ -0,0 +1,70 @@
+window.onload = function (){
+    console.log("网页可见区域宽：" + document.body.clientWidth);
+    console.log("网页可见区域高：" + document.body.clientHeight);
+    console.log("网页可见区域宽：（包括边线的宽）" + document.body.offsetWidth);
+    console.log("网页可见区域高：（包括边线的宽）" + document.body.offsetHeight);
+    console.log("网页正文全文宽：" + document.body.scrollWidth);
+    console.log("网页正文全文高：" + document.body.scrollHeight);
+    console.log("网页被卷去的高：" + document.body.scrollTop);
+    console.log("网页被卷去的左：" + document.body.scrollLeft);
+    console.log("网页正文部分上：" + window.screenTop);
+    console.log("网页正文部分左：" + Window.scrollLeft);
+    console.log("屏幕分辨率的高：" + window.screen.height);
+    console.log("屏幕分辨率的宽：" + window.screen.width);
+    console.log("屏幕可用工作区高度：" + window.screen.availHeight);
+    console.log("屏幕可用工作区宽度：" + window.screen.availWidth);
+
+
+    var page1 = document.getElementById("page1");
+    var page2 = document.getElementById("page2");
+    var page3 = document.getElementById("page3");
+
+    var music = document.getElementById("music");
+    var audio = document.getElementsByTagName("audio")[0];
+    //当音乐播放完停止时候，自动停止光盘旋转效果
+    //通过ended监听事件
+     audio.addEventListener("ended",function(event){
+         music.setAttribute("class","");
+         //music.style.webkitAnimationPlayState = "paused";
+     }, false);
+
+    //点击音乐图标，控制音乐播放效果
+//     music.onclick = function(){
+//         if (audio.paused) {
+//             audio.play();    //如果paused暂停的我们就让play播放
+//                this.setAttribute("class","play"); //控制play值为播放
+//             //    this.style.animationPlayState ="running"; //播放
+//                 //    animationPlayState 存在兼容性问题建议不用
+//                 //   webkitAnimationPlayState 除了苹果6兼容，其它的不兼容
+//         }else{
+//          audio.pause(); //音乐暂停
+//               this.setAttribute("class","");     //否则让它为空是指播放器不旋转
+//             //  this.style.animationPlayState = "paused"; //暂停
+//              //    animationPlayState 存在兼容性问题建议不用
+//                 //   webkitAnimationPlayState 除了苹果6兼容，其它的不兼
+//                  //music.style.webkitAnimationPlayState = "paused";不兼容问题
+//         };
+//    };
+//手机移动端
+   music.addEventListener("touchstart",function(event){
+       if (audio.paused) {
+            audio.play();    //如果paused暂停的我们就让play播放
+               this.setAttribute("class","play"); //控制play值为播放
+        }else{
+         audio.pause(); //音乐暂停
+              this.setAttribute("class","");     //否则让它为空是指播放器不旋转
+        };
+   },false);
+   //点击屏幕。开启好运2018，有翻页效果
+ page1.addEventListener("touchstart",function(event){
+       page1.style.display = "none";
+       page2.style.display = "block";
+       page3.style.display = "block";
+       page3.style.top     = "100%";
+       setTimeout(function() {   
+           page2.setAttribute("class","page fadeOut");
+           page3.setAttribute("class","page fadeIn");
+       }, 5500);
+    // alert('fffff')调试的时候需要
+   }, false);
+};
